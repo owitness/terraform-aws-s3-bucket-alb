28b10ad9a76a5196b7e142a8a0f167e94acd20d4
owitness <lilmacalbee@gmail.com>
2025-12-12 11:08:53 -0500

    trying to fix module not showing

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "bucket_id" {
-  value = aws_s3_bucket.this.id
+  description = "The name of the bucket"
+  value       = aws_s3_bucket.this.id
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.this.arn
+  description = "The ARN of the bucket"
+  value       = aws_s3_bucket.this.arn
 }
